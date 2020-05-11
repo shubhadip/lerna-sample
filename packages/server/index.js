d5f6dccb2fe2352c532d4944d84b86aee8b7a6d4
shubhadip <shubhadip.maity@gmail.com>
2020-05-11 12:05:50 +0530

    lerna stuff changes

--- packages/server/index.js
+++ packages/server/index.js
@@ -2,3 +2,6 @@ const commonFunction = require("@test/common");
 
 console.log(commonFunction.test1());
 console.log(commonFunction.test2());
+
+console.log(commonFunction.test2());
+console.log(commonFunction.test2());
