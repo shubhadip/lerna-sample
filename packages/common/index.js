793a5df5941a49c9bba8da431041923c1c59ca01
shubhadip <shubhadip.maity@gmail.com>
2020-05-11 12:08:39 +0530

    lerna common stuff

--- packages/common/index.js
+++ packages/common/index.js
@@ -6,8 +6,13 @@ const function2 = () => {
   console.log('function2');
 }
 
+const function3 = () => {
+  console.log('function3');
+}
+
 
 module.exports =  {
   "test1": function1,
-  "test2": function2
+  "test2": function2,
+  "test3": function3
 }
