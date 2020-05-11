b1123cd9bea52f4261b702e9dbd83e2393695e82
shubhadip <shubhadip.maity@gmail.com>
2020-05-11 12:03:35 +0530

    lerna stuff changes

--- packages/common/index.js
+++ packages/common/index.js
@@ -2,6 +2,12 @@ const function1 = () => {
   console.log('function1');
 }
 
+const function2 = () => {
+  console.log('function2');
+}
+
+
 module.exports =  {
   "test1": function1,
+  "test2": function2
 }
--- packages/server/index.js
+++ packages/server/index.js
@@ -1,2 +1,4 @@
 const commonFunction = require("@test/common");
+
 console.log(commonFunction.test1());
+console.log(commonFunction.test2());
